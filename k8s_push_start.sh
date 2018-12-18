0c7810c02533745ac5dccb1100e3e360ffdea10f
Charles Moulliard <cmoulliard@redhat.com>
2018-12-18 10:17:20 +0100

    Replace rsh with exec as it does not exist using kubectl client

--- k8s_push_start.sh
+++ k8s_push_start.sh
@@ -3,21 +3,23 @@
 component=$1
 runtime=$2
 project=$component-$runtime
+namespace=my-spring-app
 
-pod_name=$(kubectl get pod -lapp=${project} -o name)
+pod_name=$(kubectl get pod -lapp=${project} -o name -n ${namespace})
+pod_id=${pod_name#"pod/"}
 name=$(cut -d'/' -f2 <<<$pod_name)
 
 echo "## $runtime files ${project} pushed ..."
 
 if [ $runtime = "nodejs" ]; then
   cmd="run-node"
-  kubectl rsync $project/ $name:/opt/app-root/src/ --no-perms=true
+  kubectl rsync $project/ $name:/opt/app-root/src/ --no-perms=true -n ${namespace}
 else
   cmd="run-java"
-  kubectl cp ${project}/target/${project}-0.0.1-SNAPSHOT.jar $name:/deployments/app.jar
+  kubectl cp ${project}/target/${project}-0.0.1-SNAPSHOT.jar $name:/deployments/app.jar -n ${namespace}
 fi
 
-kubectl rsh $pod_name /var/lib/supervisord/bin/supervisord ctl stop $cmd
-kubectl rsh $pod_name /var/lib/supervisord/bin/supervisord ctl start $cmd
+kubectl exec $pod_id -n ${namespace} /var/lib/supervisord/bin/supervisord ctl stop $cmd
+kubectl exec $pod_id -n ${namespace} /var/lib/supervisord/bin/supervisord ctl start $cmd
 
 echo "## component ${component} (re)started"
