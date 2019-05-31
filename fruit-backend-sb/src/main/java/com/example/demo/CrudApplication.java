225f2b4d606bbcc1a8bd18f69fefae66debae681
Chris Laprun <metacosm@gmail.com>
2019-05-31 22:20:15 +0200

    feat: update to latest ap4k with new Link annotation

--- fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
+++ fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
@@ -18,6 +18,7 @@ package com.example.demo;
 
 import io.ap4k.component.annotation.CompositeApplication;
 import io.ap4k.component.annotation.Link;
+import io.ap4k.component.model.Kind;
 import io.ap4k.kubernetes.annotation.Annotation;
 import io.ap4k.kubernetes.annotation.Env;
 import io.ap4k.kubernetes.annotation.KubernetesApplication;
@@ -28,45 +29,46 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @KubernetesApplication(
-        annotations = {
-                @Annotation(key = "app.openshift.io/artifact-copy-args", value = "*.jar"),
-                @Annotation(key = "app.openshift.io/component-name", value = "fruit-backend-sb"),
-                @Annotation(key = "app.openshift.io/git-dir", value = "fruit-backend-sb"),
-                @Annotation(key = "app.openshift.io/git-ref", value = "master"),
-                @Annotation(key = "app.openshift.io/git-uri", value = "https://github.com/snowdrop/component-operator-demo.git"),
-                @Annotation(key = "app.openshift.io/java-app-jar", value = "fruit-backend-sb-0.0.1-SNAPSHOT.jar"),
-                @Annotation(key = "app.openshift.io/runtime-image", value = "fruit-backend-sb")
-        })
+    annotations = {
+        @Annotation(key = "app.openshift.io/artifact-copy-args", value = "*.jar"),
+        @Annotation(key = "app.openshift.io/component-name", value = "fruit-backend-sb"),
+        @Annotation(key = "app.openshift.io/git-dir", value = "fruit-backend-sb"),
+        @Annotation(key = "app.openshift.io/git-ref", value = "master"),
+        @Annotation(key = "app.openshift.io/git-uri", value = "https://github.com/snowdrop/component-operator-demo.git"),
+        @Annotation(key = "app.openshift.io/java-app-jar", value = "fruit-backend-sb-0.0.1-SNAPSHOT.jar"),
+        @Annotation(key = "app.openshift.io/runtime-image", value = "fruit-backend-sb")
+    })
 @CompositeApplication(
-        name = "fruit-backend-sb",
-        exposeService = true,
-        envVars = @Env(
-                name = "SPRING_PROFILES_ACTIVE",
-                value = "openshift-catalog"),
-        links = @Link(
-                name = "Secret to be injected as EnvVar using Service's secret",
-                targetcomponentname = "fruit-backend-sb",
-                kind = "Secret",
-                ref = "postgresql-db"))
+    name = "fruit-backend-sb",
+    exposeService = true,
+    envVars = @Env(
+        name = "SPRING_PROFILES_ACTIVE",
+        value = "openshift-catalog")
+)
 @ServiceCatalog(
-   instances = @ServiceCatalogInstance(
+    instances = @ServiceCatalogInstance(
         name = "postgresql-db",
         serviceClass = "dh-postgresql-apb",
         servicePlan = "dev",
         bindingSecret = "postgresql-db",
         parameters = {
-                @Parameter(key = "postgresql_user", value = "luke"),
-                @Parameter(key = "postgresql_password", value = "secret"),
-                @Parameter(key = "postgresql_database", value = "my_data"),
-                @Parameter(key = "postgresql_version", value = "9.6")
+            @Parameter(key = "postgresql_user", value = "luke"),
+            @Parameter(key = "postgresql_password", value = "secret"),
+            @Parameter(key = "postgresql_database", value = "my_data"),
+            @Parameter(key = "postgresql_version", value = "9.6")
         }
-   )
+    )
 )
+@Link(
+    name = "Secret to be injected as EnvVar using Service's secret",
+    targetcomponentname = "fruit-backend-sb",
+    kind = Kind.Secret,
+    ref = "postgresql-db")
 @SpringBootApplication
 public class CrudApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
     }
-
+    
 }
--- fruit-client-sb/src/main/java/com/example/demo/Application.java
+++ fruit-client-sb/src/main/java/com/example/demo/Application.java
@@ -17,6 +17,7 @@ package com.example.demo;
 
 import io.ap4k.component.annotation.CompositeApplication;
 import io.ap4k.component.annotation.Link;
+import io.ap4k.component.model.Kind;
 import io.ap4k.kubernetes.annotation.Annotation;
 import io.ap4k.kubernetes.annotation.Env;
 import io.ap4k.kubernetes.annotation.KubernetesApplication;
@@ -28,28 +29,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @KubernetesApplication(
-        annotations = {
-                @Annotation(key = "app.openshift.io/artifact-copy-args", value = "*.jar"),
-                @Annotation(key = "app.openshift.io/component-name", value = "fruit-client-sb"),
-                @Annotation(key = "app.openshift.io/git-dir", value = "fruit-client-sb"),
-                @Annotation(key = "app.openshift.io/git-ref", value = "master"),
-                @Annotation(key = "app.openshift.io/git-uri", value = "https://github.com/snowdrop/component-operator-demo.git"),
-                @Annotation(key = "app.openshift.io/java-app-jar", value = "fruit-client-sb-0.0.1-SNAPSHOT.jar"),
-                @Annotation(key = "app.openshift.io/runtime-image", value = "fruit-client-sb")
-        })
+    annotations = {
+        @Annotation(key = "app.openshift.io/artifact-copy-args", value = "*.jar"),
+        @Annotation(key = "app.openshift.io/component-name", value = "fruit-client-sb"),
+        @Annotation(key = "app.openshift.io/git-dir", value = "fruit-client-sb"),
+        @Annotation(key = "app.openshift.io/git-ref", value = "master"),
+        @Annotation(key = "app.openshift.io/git-uri", value = "https://github.com/snowdrop/component-operator-demo.git"),
+        @Annotation(key = "app.openshift.io/java-app-jar", value = "fruit-client-sb-0.0.1-SNAPSHOT.jar"),
+        @Annotation(key = "app.openshift.io/runtime-image", value = "fruit-client-sb")
+    })
 @CompositeApplication(
-        name = "fruit-client-sb",
-        exposeService = true,
-        links = @Link(
-                  name = "Env var to be injected within the target component -> fruit-backend",
-                  targetcomponentname = "fruit-client-sb",
-                  kind = "Env",
-                  ref = "",
-                  envVars = @Env(
-                          name  = "OPENSHIFT_ENDPOINT_BACKEND",
-                          value = "http://fruit-backend-sb:8080/api/fruits"
-                  )
-))
+    name = "fruit-client-sb",
+    exposeService = true
+)
+@Link(
+    name = "Env var to be injected within the target component -> fruit-backend",
+    targetcomponentname = "fruit-client-sb",
+    kind = Kind.Env,
+    envVars = @Env(
+        name = "OPENSHIFT_ENDPOINT_BACKEND",
+        value = "http://fruit-backend-sb:8080/api/fruits"
+    )
+)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
