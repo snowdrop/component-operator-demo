2e740abde517a7b6af6bfcae0dd7d09be252c54a
Charles Moulliard <cmoulliard@redhat.com>
2019-06-07 23:12:30 +0200

    CI : Fix issue to get running pod name

--- scripts/k8s_push_start.sh
+++ scripts/k8s_push_start.sh
@@ -7,18 +7,16 @@ runtime=$2
 project=$component-$runtime
 namespace=${3:-my-spring-app}
 
-pod_name=$(kubectl get pod -lapp=${project} -o name -n ${namespace} | grep "Running")
-pod_id=${pod_name#"pod/"}
-name=$(cut -d'/' -f2 <<<$pod_name)
+pod_id=$(kubectl get pod -lapp=${project} -n ${namespace} | grep "Running" | awk '{print $1}')
 
 echo "## $runtime files ${project} pushed ..."
 
 if [ $runtime = "nodejs" ]; then
   cmd="run-node"
-  kubectl rsync $project/ $name:/opt/app-root/src/ --no-perms=true -n ${namespace}
+  kubectl rsync $project/ $pod_id:/opt/app-root/src/ --no-perms=true -n ${namespace}
 else
   cmd="run-java"
-  kubectl cp ${project}/target/${project}-0.0.1-SNAPSHOT.jar $name:/deployments/app.jar -n ${namespace}
+  kubectl cp ${project}/target/${project}-0.0.1-SNAPSHOT.jar $pod_id:/deployments/app.jar -n ${namespace}
 fi
 
 kubectl exec $pod_id -n ${namespace} /var/lib/supervisord/bin/supervisord ctl stop $cmd
