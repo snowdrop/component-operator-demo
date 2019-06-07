697c845b7aba761de4f7db10c88b770e8d9dcb1b
Charles Moulliard <cmoulliard@redhat.com>
2019-06-07 18:48:03 +0200

    CI : Move grep within parenthesis

--- scripts/k8s_push_start.sh
+++ scripts/k8s_push_start.sh
@@ -7,7 +7,7 @@ runtime=$2
 project=$component-$runtime
 namespace=${3:-my-spring-app}
 
-pod_name=$(kubectl get pod -lapp=${project} -o name -n ${namespace}) | grep "Running"
+pod_name=$(kubectl get pod -lapp=${project} -o name -n ${namespace} | grep "Running")
 pod_id=${pod_name#"pod/"}
 name=$(cut -d'/' -f2 <<<$pod_name)
 
