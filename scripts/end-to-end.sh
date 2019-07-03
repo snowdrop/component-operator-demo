462fb94abb78067fe2c52e9aa67b7d0cd2ebe14f
Charles Moulliard <cmoulliard@redhat.com>
2019-07-03 15:03:37 +0200

    Remove non used resources needed to use OABroker

--- scripts/end-to-end.sh
+++ scripts/end-to-end.sh
@@ -39,14 +39,14 @@ printTitle "Report status : ${TIME}" > ${REPORT_FILE}
 
 printTitle "1. Status of the resources created using the CRDs : Component, Link or Capability" >> ${REPORT_FILE}
 if [ "$INGRESS_RESOURCES" == "No resources found." ]; then
-  for i in components links capabilities pods deployments deploymentconfigs services routes pvc serviceinstances servicebindings secret/postgresql-db
+  for i in components links capabilities pods deployments deploymentconfigs services routes pvc postgreses secret/postgres-db-config
   do
     printTitle "$(echo $i | tr a-z A-Z)" >> ${REPORT_FILE}
     kubectl get $i -n demo >> ${REPORT_FILE}
     printf "\n" >> ${REPORT_FILE}
   done
 else
-  for i in components links capabilities pods deployments servicces ingresses pvc serviceinstances servicebindings secret/postgresql-db
+  for i in components links capabilities pods deployments servicces ingresses pvc postgreses secret/postgres-db-config
   do
     printTitle "$(echo $i | tr a-z A-Z)" >> ${REPORT_FILE}
     kubectl get $i -n demo >> ${REPORT_FILE}
@@ -64,7 +64,7 @@ printTitle "3. ENV var defined for the fruit client component" >> ${REPORT_FILE}
 for item in $(kubectl get pod -n demo -lapp=fruit-client-sb --output=name); do printf "Envs for %s\n" "$item" | grep --color -E '[^/]+$' && kubectl get "$item" --output=json | jq -r -S '.spec.containers[0].env[] | " \(.name)=\(.value)"' 2>/dev/null; printf "\n"; done >> ${REPORT_FILE}
 printf "\n" >> ${REPORT_FILE}
 
-printTitle "Push fruit and backend"
+printTitle "Push fruit client and backend"
 ./scripts/k8s_push_start.sh fruit-backend sb demo
 ./scripts/k8s_push_start.sh fruit-client sb demo
 
