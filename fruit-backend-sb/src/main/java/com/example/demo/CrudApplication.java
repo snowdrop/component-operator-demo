3515cbc0b5ebb178e4a4774766ea0387be8cb5fd
Charles Moulliard <cmoulliard@redhat.com>
2019-07-03 15:27:12 +0200

    Remove non needed annotation. Add missing maven profile to inject postgresql driver during build

--- fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
+++ fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
@@ -20,9 +20,6 @@ import io.ap4k.component.annotation.CompositeApplication;
 import io.ap4k.component.annotation.Link;
 import io.ap4k.component.model.Kind;
 import io.ap4k.kubernetes.annotation.Env;
-import io.ap4k.servicecatalog.annotation.Parameter;
-import io.ap4k.servicecatalog.annotation.ServiceCatalog;
-import io.ap4k.servicecatalog.annotation.ServiceCatalogInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -32,20 +29,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     envs = @Env(
         name = "SPRING_PROFILES_ACTIVE",
         value = "broker-catalog")
-)
-@ServiceCatalog(
-    instances = @ServiceCatalogInstance(
-        name = "postgresql-db",
-        serviceClass = "dh-postgresql-apb",
-        servicePlan = "dev",
-        bindingSecret = "postgresql-db",
-        parameters = {
-            @Parameter(key = "postgresql_user", value = "luke"),
-            @Parameter(key = "postgresql_password", value = "secret"),
-            @Parameter(key = "postgresql_database", value = "my_data"),
-            @Parameter(key = "postgresql_version", value = "9.6")
-        }
-    )
 )
 @Link(
     name = "link-to-database",
