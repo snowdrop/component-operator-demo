eccd03133b95936381ac42313486ba7ee8da6ed2
Charles Moulliard <cmoulliard@redhat.com>
2019-07-04 10:04:05 +0200

    Increase sleep time. Move deletion of the resource end of the script

--- scripts/end-to-end_dummy_resources.sh
+++ scripts/end-to-end_dummy_resources.sh
@@ -10,7 +10,7 @@
 #
 CLUSTER_IP=${1:-$(minikube ip)}
 NS=${2:-test1}
-SLEEP_TIME=20s
+SLEEP_TIME=30s
 TIME=$(date +"%Y-%m-%d_%H-%M")
 REPORT_FILE="result_${TIME}.txt"
 EXPECTED_RESPONSE='{"status":"UP"}'
@@ -22,11 +22,9 @@ function printTitle {
   printf "$r\n$1\n$r\n"
 }
 
+
+printTitle "Creating the namespace"
 kubectl create ns ${NS}
-printTitle "Delete the resources components, links and capabilities"
-kubectl delete components,links,capabilities --all -n ${NS}
-echo "Sleep ${SLEEP_TIME}"
-sleep ${SLEEP_TIME}
 
 printTitle "Deploy the component for the fruit-backend, link and capability"
 cat <<EOF | kubectl apply -n ${NS} -f -
@@ -144,12 +142,12 @@ printTitle "Push fruit client and backend"
 echo "Wait until Spring Boot actuator health replies UP for both microservices"
 for i in fruit-backend-sb fruit-client-sb
   do
-    HTTP_RESPONSE=$(kubectl exec -n ${NS} $(kubectl get pod -n ${NS} -lapp=$i | grep "Running" | awk '{print $1}') -- curl -L -w "HTTPSTATUS:%{http_code}" -s localhost:8080/actuator/health 2>&1)
+    HTTP_RESPONSE=$(kubectl exec -n $NS $(kubectl get pod -n $NS -lapp=$i | grep "Running" | awk '{print $1}') -- curl -L -w "HTTPSTATUS:%{http_code}" -s localhost:8080/actuator/health 2>&1)
     HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
     until [ "$HTTP_BODY" == "$EXPECTED_RESPONSE" ]; do
       echo "Wait 5s ...."
       echo "$i: Response is : $HTTP_BODY, expected is : $EXPECTED_RESPONSE"
-      sleep 5s
+      sleep 10s
     done
 done
 
@@ -165,4 +163,10 @@ else
     curl -H "Host: fruit-client-sb" ${FRONTEND_ROUTE_URL}/api/client >> ${REPORT_FILE}
 fi
 
+printTitle "Delete the resources components, links and capabilities"
+kubectl delete components,links,capabilities --all -n ${NS}
+kubectl delete service/kubedb secret/postgres-db-config -n test1
+
+echo "Sleep ${SLEEP_TIME}"
+sleep ${SLEEP_TIME}
 kubectl delete ns ${NS}
